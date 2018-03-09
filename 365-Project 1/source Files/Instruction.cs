d0ef4f222a2bb3ef6c1920b4d272b6ba1a1b75fa
Brent Hurst <seqvalley77@aol.com>
2018-03-08 19:16:27 -0500

    Instruction.cs created and written (first pass, at least). Also committed my header file in case y'all wanna use it. No obligation but I'm too lazy to keep writing the same thing over and over and I figured yall might be too.

---
+++ 365-Project 1/source Files/Instruction.cs
@@ -0,0 +1,31 @@
+/*************************************************
+ *   CS365 SP18 Project1
+ *   Group 1D
+ *       Austin Saporito
+ *       G. Brent Hurst
+ *       Kendall Nicley
+ *
+ *   Instruction.cs
+ *
+ *   Interface for each Instruction
+ *
+ ************************************************/
+
+
+interface IInstruction
+{
+	//"pop" or "add", etc
+	//includes "set" so Brent can change to lowercase
+	string Str {get; set;}
+
+	//the ByteCode to be output for this instruction
+	uint Encoded {get; set;}
+
+	//Wasn't sure whether the parameter(s) should go
+	//in a property or a public variable so I'm just
+	//not including it here
+
+	void SetInstructionFromLine(string line);
+	void EncodeIntruction();
+	void WriteInstructionByteCode();
+}
