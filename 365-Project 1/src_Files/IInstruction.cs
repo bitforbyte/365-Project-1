e50df35d69545215605e60b8f184647907a8a6b6
Brent Hurst <seqvalley77@aol.com>
2018-03-21 12:34:48 -0400

    IInstruction and Instruction

---
+++ 365-Project 1/src_Files/IInstruction.cs
@@ -0,0 +1,23 @@
+/*************************************************
+ *   CS365 SP18 Project1
+ *   Group 1D
+ *       Austin Saporito
+ *       G. Brent Hurst
+ *       Kendall Nicley
+ *
+ *   Instruction.cs
+ *
+ *   Interface for each Instruction
+ *
+ ************************************************/
+
+
+interface IInstruction
+{
+	//"pop" or "add", etc
+	//includes "set" so Brent can change to lowercase
+	string Str {get; set;}
+
+	//the ByteCode to be output for this instruction
+	uint Encoded {get;}
+}
--- 365-Project 1/src_Files/Instruction.cs
+++ 365-Project 1/src_Files/Instruction.cs
@@ -1,31 +1,15 @@
-/*************************************************
- *   CS365 SP18 Project1
- *   Group 1D
- *       Austin Saporito
- *       G. Brent Hurst
- *       Kendall Nicley 
- *
- *   Instruction.cs
- *
- *   Interface for each Instruction
- *
- ************************************************/
-
-
-interface IInstruction
+class Instruction : IInstruction
 {
-	//"pop" or "add", etc
-	//includes "set" so Brent can change to lowercase
-	string Str {get; set;}
+	string mStr;
+	string Str
+	{
+		get { return mStr; }
+		set { mStr = value; }
+	}
 
-	//the ByteCode to be output for this instruction
-	uint Encoded {get; set;}
-
-	//Wasn't sure whether the parameter(s) should go
-	//in a property or a public variable so I'm just
-	//not including it here
-
-	void SetInstrFromLine(string line);
-	void EncodeInstr();
-	void WriteInstrByteCode();
+	uint mEncoded;
+	uint Encoded
+	{
+		get { return mEncoded; }
+	}
 }
