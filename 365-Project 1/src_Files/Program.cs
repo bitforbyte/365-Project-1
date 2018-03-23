e503ea82df9a8e0a4c182c479694ec475184ec86
austin saporito <austincsp@yahoo.com>
2018-03-22 21:30:50 -0400

    reads file

--- 365-Project 1/src_Files/Program.cs
+++ 365-Project 1/src_Files/Program.cs
@@ -1,5 +1,6 @@
 ﻿//
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,20 +8,44 @@ using System.Threading.Tasks;
 
 namespace _365_Project_1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
 			Assembler assem = new Assembler();
 			string cmd = "swap";
 			string test = "swap info";
-
+			Program pro=new Program();
+			pro.reader(args[0]);
 			foreach(var item in assem.delDic)
 			{
 				assem.delDic[item.Key].DynamicInvoke(item.Key);
 			}
 
+		}
+		public int reader(string file){
+			 string line;
 
+        if(File.Exists(file)){
+            using (var read=new StreamReader(File.OpenRead(file))){
+                while((line=read.ReadLine())!=null){
+                    if(line.StartsWith("//")||line==string.Empty||line.StartsWith("#")){
+                        //skip
+                    }else{
+                      // Console.WriteLine(line);
+						Instruction inter= new Instruction();
+						inter.Line=line;
+
+
+                    }
+
+                }
+            }
         }
-    }
+        return 1;
+		}
+
+
+
+	}
 }
