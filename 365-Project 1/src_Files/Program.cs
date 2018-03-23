3fe4ce6cf77355043bbad0b60e144930767823de
austin saporito <austincsp@yahoo.com>
2018-03-23 15:39:08 -0400

    im tired of comments

--- 365-Project 1/src_Files/Program.cs
+++ 365-Project 1/src_Files/Program.cs
@@ -18,9 +18,9 @@ namespace _365_Project_1
 			string test = "swap info";
 			Program pro=new Program();
 			Ilist=pro.reader(args[0]);
-			foreach(var item in assem.delDic)
+			foreach(var i in Ilist)
 			{
-				assem.delDic[item.Key].DynamicInvoke(item.Key);
+				assem.delDic[i.Cmd].DynamicInvoke(i);
 			}
 
 		}
@@ -30,39 +30,41 @@ namespace _365_Project_1
 			List<Instruction>Ilist=new List<Instruction>();
 			Dictionary<Label,string> dic=new Dictionary<Label,string>();;
 
-		if(File.Exists(file)){
-            using (var read=new StreamReader(File.OpenRead(file))){
-                while((line=read.ReadLine())!=null){
-                    if(line.StartsWith("//")||line==string.Empty||line.StartsWith("#")){
-                        //skip
-                    }else if(line.EndsWith(":")&&addr!=0){
-						addr+=4;
-						Label la=new Label();
-						la.labelName=line;
-						la.Addr=addr;
-						dic.Add(la,line);
-					}else{
-						addr+=4;
+			if(File.Exists(file)){
+				using (var read=new StreamReader(File.OpenRead(file))){
+					while((line=read.ReadLine())!=null){
+						if(line.StartsWith("//")||line==string.Empty||line.StartsWith("#")){
+							//skip
+						}else if(line.EndsWith(":")&&addr!=0){
+							addr+=4;
+							Label la=new Label();
+							la.labelName=line;
+							la.Addr=addr;
+							dic.Add(la,line);
+							Console.WriteLine(line);
+							Console.WriteLine(addr);
+						}else{
+							addr+=4;
+						}
+					}
 				}
 			}
-		}
-		}
-        if(File.Exists(file)){
-            using (var read=new StreamReader(File.OpenRead(file))){
-                while((line=read.ReadLine())!=null){
-                    if(line.StartsWith("//")||line==string.Empty||line.StartsWith("#")){
-                        //skip
-                    }else{
-						Instruction inter= new Instruction();
-						inter.Line=line;
-						Ilist.Add(inter);
-						Console.WriteLine(inter.Val);
-                    }
+			if(File.Exists(file)){
+				using (var read=new StreamReader(File.OpenRead(file))){
+					while((line=read.ReadLine())!=null){
+						if(line.StartsWith("//")||line==string.Empty||line.StartsWith("#")){
+							//skip
+						}else{
+							Instruction inter= new Instruction();
+							inter.Line=line;
+							Ilist.Add(inter);
+							Console.WriteLine(inter.Val);
+						}
 
-                }
-            }
-        }
-        return Ilist;
+					}
+				}
+			}
+			return Ilist;
 		}
 
 
