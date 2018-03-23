3de4fad42e55cdd59e3c46cedd7400194fb494d4
austin saporito <austincsp@yahoo.com>
2018-03-23 14:26:04 -0400

    added interface for label

---
+++ 365-Project 1/src_Files/Ilabel.cs
@@ -0,0 +1,21 @@
+/*************************************************
+ *   CS365 SP18 Project1
+ *   Group 1D
+ *       Austin Saporito
+ *       G. Brent Hurst
+ *       Kendall Nicley
+ *
+ *   Ilabel.cs
+ *
+ *   Interface for each label
+ *
+ ************************************************/
+
+using _365_Project_1;
+
+public interface Ilabel
+{
+    string labelName {get; set;}
+    uint Addr {get; set;}
+}
+
---
+++ 365-Project 1/src_Files/Label.cs
@@ -0,0 +1,33 @@
+/*************************************************
+ *   CS365 SP18 Project1
+ *   Group 1D
+ *       Austin Saporito
+ *       G. Brent Hurst
+ *       Kendall Nicley
+ *
+ *   Label.cs
+ *
+ *   Class for each Label.
+ *   Inherits ILabel.
+ *
+ ************************************************/
+using System;
+using _365_Project_1;
+
+public class Label : Ilabel
+{
+
+	public string mlabel;
+	public string labelName{
+		get{ return mlabel; }
+		
+		set{ mlabel=value; }
+
+	}
+	public uint maddr;
+	public uint Addr{
+		get{ return maddr; }
+		set{ maddr = value; }
+	}
+
+}
