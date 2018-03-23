438942f809d6ab53bf1380635d4a3b43504e357c
austin saporito <austincsp@yahoo.com>
2018-03-22 22:59:36 -0400

    aAssembler.cs done

--- 365-Project 1/src_Files/aAssembler.cs
+++ 365-Project 1/src_Files/aAssembler.cs
@@ -5,8 +5,56 @@ using _365_Project_1;
 
 partial class Assembler
 {
+	uint strbyte;
+
 	void Add(Instruction i)
 	{
+		strbyte=2<<28;
+		i.Encoded=strbyte;
+		return;
+	}
+	void Sub(Instruction i)
+	{
+		strbyte=33<<24;
+		i.Encoded=strbyte;
+		return;
+	}
+
+	void Mul(Instruction i){
+
+		strbyte=34<<24;
+		i.Encoded=strbyte;
+		return;
+
+	}
+	void Div(Instruction i)
+	{
+		strbyte=35<<24;
+		i.Encoded=strbyte;
+		return;
+	}
+	void Rem(Instruction i)
+	{
+		strbyte=36<<24;
+		i.Encoded=strbyte;
+		return;
+	}
+	void And(Instruction i)
+	{
+		strbyte=37<<24;
+		i.Encoded=strbyte;
+		return;
+	}
+	void Or(Instruction i)
+	{
+		strbyte=38<<24;
+		i.Encoded=strbyte;
+		return;
+	}
+	void Xor(Instruction i)
+	{
+		strbyte=39<<24;
+		i.Encoded=strbyte;
 		return;
 	}
 }
